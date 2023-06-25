ade716e67799057d3436987c155f6e9968d97375
Sarthak <teotiasarthak25@gmail.com>
2023-06-25 21:37:44 +0530

    first commit

---
+++ src/RegistrationForm.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Replace this with your desired logic to handle the form data
+    // For example, you can make an API call to register the user
+    // or display a success message
+    console.log(formData);
+    // Reset the form data
+    setFormData({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name:
+        <input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Last Name:
+        <input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default RegistrationForm;
